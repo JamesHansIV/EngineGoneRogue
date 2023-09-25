3848f55efbe452bce8e595e8ceff18f5f45110b0
Minwoo Lee <minwoolee@Minwoos-MacBook-Air.local>
2023-09-24 19:37:29 -0500

    Add minwoo tutorials

---
+++ minwoo/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+using namespace std;
+int main() {
+        cout << "Hello World!";
+        SDL_Init(SDL_INIT_VIDEO);
+        return 0;
+    }
---
+++ minwoo/makefile
@@ -0,0 +1,2 @@
+game:
+	g++ main.cpp -o play -I include -L lib -l SDL2-2.0.0
