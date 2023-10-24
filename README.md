ba1c0cf125f4d4e900c544a12f8ddb14e0233ee7
Minwoo Lee <leejoseph892@gmail.com>
2023-10-24 14:18:11 -0500

    Moved src dir and included imgui

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Each folder is the name of a member, with their finished artifacts inside.
 For sprint 1, there will be C++ SDL2 code that should create a small window that accepts keyboard input, and a paragraph explaining what a game engine is and the components that make up a game engine. The paragraph should also describe how each component works.
 
 Each  member should have a readme in their folder that explains how to compile and run their code.
+
+
+To build with cmake:
+1) % cmake -B (buildDir)
+2) % cd (buildDir)
+3) % make all
+4) % ./Engine
---
+++ src/Core/Engine.cpp
@@ -0,0 +1,75 @@
+#include "Engine.h"
+#include "../Graphics/TextureManager.h"
+
+#include "../Character/Warrior.h"
+#include "../Object/GameObject.h"
+
+#include <unistd.h>
+
+Engine* Engine::m_Instance = nullptr;
+Warrior* player = nullptr;
+
+bool Engine::Init(){
+
+    if(SDL_Init(SDL_INIT_VIDEO)!=0 && IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG)!= 0){
+        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    m_Window = SDL_CreateWindow("Soft Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(m_Window == nullptr){
+        SDL_Log("Failed to create Window: %s", SDL_GetError());
+        return false;
+    }
+
+    m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(m_Renderer == nullptr){
+        SDL_Log("Failed to create Renderer: %s", SDL_GetError());
+        return false;
+    }
+    //TODO: note that the cwd is <projectDir>/build instead of <projectDir>.
+    //      Set a working directory path macro to use absolute file paths
+    TextureManager::GetInstance()->Load("player", "../res/imgs/Idle.png");
+    player = new Warrior(new Properties("player", 0, 0, 136, 96));
+
+    m_EventHandler.addListener(*player);
+    Transform tf;
+    tf.Log();
+    return m_IsRunning = true;
+}
+
+void Engine::Update(float dt){
+    player->Update(0);
+}
+
+void Engine::Render(){
+    SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+    SDL_RenderClear(m_Renderer);
+
+    player->Draw();
+    SDL_RenderPresent(m_Renderer);
+}
+
+void Engine::Events(){
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    if (event.type == SDL_QUIT) {
+        Quit();
+        return;
+    }
+    m_EventHandler.handleEvent(event);
+}
+
+bool Engine::Clean(){
+    //TODO: check if cleanup is successful
+    TextureManager::GetInstance()->Clean();
+    SDL_DestroyRenderer(m_Renderer);
+    SDL_DestroyWindow(m_Window);
+    IMG_Quit();
+    SDL_Quit();
+    return true;
+}
+
+void Engine::Quit(){
+    m_IsRunning = false;
+}
