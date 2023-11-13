ace6f260242996abc85a5b0f1435df6a0da1c083
ElenBhattarai <elenbhattarai2002@ku.edu>
2023-11-13 10:47:19 -0600

    refactor bad code

--- src/Engine/Objects/Weapon.cpp
+++ src/Engine/Objects/Weapon.cpp
@@ -16,27 +16,13 @@ void Weapon:: Draw()
 
 void Weapon::Update(float dt)
 {
-    int const player_x = GetMidPointX() - Renderer::GetInstance()->GetCameraX();
-    int const player_y = GetMidPointY() - Renderer::GetInstance()->GetCameraY();
-    int const mouse_x = InputChecker::GetMouseX();
-    int const mouse_y = InputChecker::GetMouseY();
-
-    // Calculate the angle between the mouse and the player
-    float const delta_x = mouse_x - player_x;
-    float const delta_y = mouse_y - player_y;
-    float angle = atan2(delta_y, delta_x) * (180.0 / M_PI);
-    // Convert the angle range from -180 to 180 to 0 to 360
-    if (angle < 0) angle += 360.0F;
-
-    SDL_Log("%f", angle);
-
     if (m_Type == PROJECTILE)
     {
-        Properties projectile_props("projectile", {0, 0, 723, 724}, {GetMidPointX(), GetMidPointY(), 10, 10}, angle);
+        Properties projectile_props("projectile", {0, 0, 723, 724}, {GetMidPointX(), GetMidPointY(), 10, 10}, GetRotation());
         if (InputChecker::IsMouseButtonPressed(SDL_BUTTON_LEFT))
         {
             Projectile* projectile = nullptr;
-            projectile = new Projectile(projectile_props, 50, 1.0, angle);
+            projectile = new Projectile(projectile_props, 50, 1.0, GetRotation());
             projectiles.push_back(projectile);
             InputChecker::SetMouseButtonPressed(SDL_BUTTON_LEFT, false);
         }
