40344e4282764ea57e0589d6e8224a46199853c5
James Hanselman <68711757+JamesHansIV@users.noreply.github.com>
2024-02-18 02:28:57 -0600

    * added ON_UP_LATCH input type * added enums and maps for Mac keys to sdl keycodes * Added copy,paste,undo,redo to EditorAction enum * added num_modifier_keys to Bind struct * converted maps to static maps * CheckInputs expanded to handle all InputTypes

--- src/Engine/KeyMap/KeyMap.cpp
+++ src/Engine/KeyMap/KeyMap.cpp
@@ -6,6 +6,8 @@
 #include<string>
 #include <utility>
 #include<vector>
+#include<unordered_map>
+#include<unordered_set>
 
 std::string path = "editor.binds";
 
@@ -22,45 +24,129 @@ std::ostream& operator << (std::ostream& os, const InputType& obj)
 }
 
 Bind KeyMap::CreateBind(EditorAction action, InputType input_type, std::vector<SDL_KeyCode> keys) {
-    Bind bind = {action, input_type, std::move(keys)};
+    int modifier_key_count = 0;
+
+    for (SDL_KeyCode key : keys) {
+        if (modifier_keys.contains(key))
+            modifier_key_count++;
+    }
+
+    Bind bind = {action, input_type, std::move(keys), modifier_key_count};
+
     return bind;
 }
 
 
 KeyMap::KeyMap() {
-    m_Default_Bindings_Map = { 
-        {EditorAction::EXIT_CURRENT_TOOL, CreateBind(EditorAction::EXIT_CURRENT_TOOL, InputType::LATCH, std::vector<SDL_KeyCode>{SDLK_ESCAPE})},
-        {EditorAction::ENTER_DRAW_TOOL, CreateBind(EditorAction::ENTER_DRAW_TOOL, InputType::LATCH, std::vector<SDL_KeyCode>{SDLK_d})}, 
-        {EditorAction::ENTER_ERASE_TOOL, CreateBind(EditorAction::ENTER_ERASE_TOOL, InputType::LATCH, std::vector<SDL_KeyCode>{SDLK_e})},
-        {EditorAction::PAN_CAMERA_DOWN, CreateBind(EditorAction::PAN_CAMERA_DOWN, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_DOWN})},
-        {EditorAction::PAN_CAMERA_UP, CreateBind(EditorAction::PAN_CAMERA_UP, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_UP})},
-        {EditorAction::PAN_CAMERA_RIGHT, CreateBind(EditorAction::PAN_CAMERA_RIGHT, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_RIGHT})},
-        {EditorAction::PAN_CAMERA_LEFT, CreateBind(EditorAction::PAN_CAMERA_LEFT, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_LEFT})}
-    };
+    std::cout << "INIT KEYBINDS\n";
+    InitModifierKeys();
 
     BuildMapFromFile();
 
-    Bind bind = m_Bindings_Map[EditorAction::ENTER_DRAW_TOOL];
-    std::cout << " " << bind.keys[0] << " " << bind.action << '\n';
-    std::cout << m_Bindings_Map.size() << '\n';
-    std::cout << m_Default_Bindings_Map.size() << '\n';
+    // std::cout << "mapped key " << mac_keys_to_sdl[MacKeys::LCOMMAND] << "\n";
+
+
+    // std::cout << "\n";
+    // for (const auto& [ action, bind ] : default_bindings) {
+    //     std::cout << "\nACTION " << action << ":";
+    //     if (static_cast<int>(action) != 10) std::cout << " ";
+    //     std::cout << "  n_mod_keys: " << bind.num_modifier_keys;
+    //     for (auto key : bind.keys) {
+    //         std::cout << "    " << key;
+    //     }
+    // }
+    // std::cout << "\n";
 }
 
 KeyMap::~KeyMap() = default;
 
+std::unordered_map<MacKeys, SDL_KeyCode> KeyMap::mac_keys_to_sdl = {
+    {MacKeys::LSHIFT, SDLK_LSHIFT}, 
+    {MacKeys::RSHIFT, SDLK_RSHIFT}, 
+    {MacKeys::CONTROL, SDLK_LCTRL}, 
+    {MacKeys::LOPTION, SDLK_LALT},  
+    {MacKeys::ROPTION, SDLK_RALT},  
+    {MacKeys::LCOMMAND, SDLK_LGUI}, 
+    {MacKeys::RCOMMAND, SDLK_RGUI}  
+};
+
+std::unordered_set<SDL_KeyCode>KeyMap::modifier_keys = InitModifierKeys();
+
+std::unordered_map<EditorAction, Bind> KeyMap::default_bindings = {
+    // Tool selection
+    {EditorAction::EXIT_CURRENT_TOOL, CreateBind(EditorAction::EXIT_CURRENT_TOOL, InputType::LATCH, std::vector<SDL_KeyCode>{SDLK_ESCAPE})},
+    {EditorAction::ENTER_DRAW_TOOL, CreateBind(EditorAction::ENTER_DRAW_TOOL, InputType::LATCH, std::vector<SDL_KeyCode>{SDLK_d})}, 
+    {EditorAction::ENTER_ERASE_TOOL, CreateBind(EditorAction::ENTER_ERASE_TOOL, InputType::LATCH, std::vector<SDL_KeyCode>{SDLK_e})},
+    
+    // Camera controls
+    {EditorAction::PAN_CAMERA_DOWN, CreateBind(EditorAction::PAN_CAMERA_DOWN, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_DOWN})},
+    {EditorAction::PAN_CAMERA_UP, CreateBind(EditorAction::PAN_CAMERA_UP, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_UP})},
+    {EditorAction::PAN_CAMERA_RIGHT, CreateBind(EditorAction::PAN_CAMERA_RIGHT, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_RIGHT})},
+    {EditorAction::PAN_CAMERA_LEFT, CreateBind(EditorAction::PAN_CAMERA_LEFT, InputType::CONTINUOUS, std::vector<SDL_KeyCode>{SDLK_LEFT})},
+
+    // Copy and paste
+    {EditorAction::COPY_SELECTION, CreateBind(EditorAction::COPY_SELECTION, InputType::LATCH, std::vector<SDL_KeyCode>{mac_keys_to_sdl[MacKeys::LCOMMAND], SDLK_c})},
+    {EditorAction::PASTE_SELECTION, CreateBind(EditorAction::PASTE_SELECTION, InputType::LATCH, std::vector<SDL_KeyCode>{mac_keys_to_sdl[MacKeys::LCOMMAND], SDLK_v})},
+
+    // Undo and Redo
+    {EditorAction::UNDO_ACTION, CreateBind(EditorAction::UNDO_ACTION, InputType::ON_UP_LATCH, std::vector<SDL_KeyCode>{mac_keys_to_sdl[MacKeys::LCOMMAND], SDLK_z})},
+    {EditorAction::REDO_ACTION, CreateBind(EditorAction::REDO_ACTION, InputType::ON_UP_LATCH, 
+                                            std::vector<SDL_KeyCode>{mac_keys_to_sdl[MacKeys::LCOMMAND], mac_keys_to_sdl[MacKeys::LSHIFT], SDLK_z})}
+};
+
+std::unordered_set<SDL_KeyCode> KeyMap::InitModifierKeys() {
+    auto keys = InputChecker::GetModifierKeyCodes();
+    std::unordered_set<SDL_KeyCode>temp = {};
+    for (int key : keys) {
+        temp.insert(static_cast<SDL_KeyCode>(key));
+    }
+
+    return temp;
+}
+
 bool KeyMap::CheckInputs(EditorAction action) {
-    bool const res = false;
     Bind const bind = m_Bindings_Map[action];
 
     for (SDL_KeyCode const key : bind.keys) {
         bool const curr_down = InputChecker::IsKeyPressed(key);
         bool const prev_down = InputChecker::WasKeyAlreadyPresssed(key);
 
-        if (bind.input_type == InputType::LATCH && prev_down || !curr_down) {
+        // check modifiers, modifiers may already be pressed
+        if (bind.num_modifier_keys != InputChecker::GetNumModifierKeysDown()) {
             return false;
         }
+
+        // if one key is a modifier key ignore the latch check (for that key)
+        if (modifier_keys.contains(key)) {
+            if (!curr_down) {
+                return false;
+            }
+                // return false;
+            continue;
+        }
+
+        // check release type inputs
+        if (bind.input_type == InputType::ON_UP_LATCH) {
+            if (curr_down || !prev_down) {
+                return false;
+            }
+        }
+
+        // check latch type inputs
+        if (bind.input_type == InputType::LATCH){
+            if (prev_down || !curr_down) {
+                return false;
+            }
+        }
+
+        // check Continuous
+        if (bind.input_type == InputType::CONTINUOUS) {
+            if (!curr_down) return false;
+        }
     }
-    // std::cout << "returning true" << std::endl;
+
+    // if (bind.input_type != InputType::CONTINUOUS)
+        // std::cout << "ACTION " << action << " SUCCCESS\n";
     return true;
 }
 
@@ -72,7 +158,7 @@ void KeyMap::BuildMapFromFile() {
         if (file.is_open()) {
             std::cout << "file open" << '\n';
         } else {
-            throw std::runtime_error("failure to open file!");
+            throw std::runtime_error("failure to open keybinds (*.binds) file: " + path);
         }
 
         // read lines and add them to line vector
@@ -114,7 +200,7 @@ void KeyMap::BuildMapFromFile() {
     catch (const std::runtime_error& e) {
         std::cout << "ERROR: " << e.what() << '\n';
         std::cout << "Using defaults\n";
-        m_Bindings_Map = m_Default_Bindings_Map;
+        m_Bindings_Map = default_bindings;
     }
 }
 
--- src/Engine/KeyMap/KeyMap.h
+++ src/Engine/KeyMap/KeyMap.h
@@ -1,14 +1,17 @@
 #pragma once
 
 #include<SDL2/SDL.h>
-#include<map>
 #include<vector>
+#include<unordered_set>
+#include<unordered_map>
 
 // add default map 
 // add save to map file
 // add load from map file (one editor load and file modified);
 
-enum class InputType { CONTINUOUS, LATCH };
+enum class InputType { CONTINUOUS, LATCH, ON_UP_LATCH};
+
+enum class MacKeys { LSHIFT, RSHIFT, CONTROL, LOPTION, ROPTION, LCOMMAND, RCOMMAND };
 
 enum class EditorAction { 
     EXIT_CURRENT_TOOL, 
@@ -17,13 +20,18 @@ enum class EditorAction {
     PAN_CAMERA_UP,
     PAN_CAMERA_RIGHT,
     PAN_CAMERA_LEFT, 
-    PAN_CAMERA_DOWN
+    PAN_CAMERA_DOWN,
+    COPY_SELECTION,
+    PASTE_SELECTION,
+    UNDO_ACTION,
+    REDO_ACTION
 };
 
 struct Bind {
     EditorAction action;
     InputType input_type;
     std::vector<SDL_KeyCode> keys;
+    int num_modifier_keys;
 };
 
 class KeyMap {
@@ -31,8 +39,9 @@ class KeyMap {
         KeyMap();
         ~KeyMap();
 
-        std::map<EditorAction, Bind> m_Bindings_Map;
-        std::map<EditorAction, Bind> m_Default_Bindings_Map;
+        static std::unordered_set<SDL_KeyCode>modifier_keys;
+        static std::unordered_map<MacKeys, SDL_KeyCode>mac_keys_to_sdl;
+        static std::unordered_map<EditorAction, Bind>default_bindings;
 
         void BuildMapFromFile();
         bool CheckInputs(EditorAction action);
@@ -44,4 +53,10 @@ class KeyMap {
 
         static Bind CreateBind(EditorAction action, InputType input_type, std::vector<SDL_KeyCode> keys);
 
+    private:
+        std::unordered_map<EditorAction, Bind> m_Bindings_Map;
+        // std::map<EditorAction, Bind> m_Default_Bindings_Map;
+
+        static std::unordered_set<SDL_KeyCode>InitModifierKeys();
+        void InitMacKeysToSDLMap();
 };
