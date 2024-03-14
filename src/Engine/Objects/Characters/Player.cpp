65a26f1b7538f27f4765c7ebf660cdb54e1e974d
Ahmni <ahmni@ku.edu>
2024-03-14 00:23:30 -0400

    Make Experience gain slower on level up

--- src/Engine/Objects/Characters/Player.cpp
+++ src/Engine/Objects/Characters/Player.cpp
@@ -17,7 +17,7 @@ const int kIdleAnimationSpeed = 50;
 const int kProjectileWidth = 10;
 const int kProjectileHeight = 10;
 // Experience multiplier for each level
-double k_experience_multiplier = 9.0 / 10.0;
+double k_experience_multiplier = 7.5 / 10.0;
 
 Player::Player(Properties& props) : Character(props) {
     Init();
