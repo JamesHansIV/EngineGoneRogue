2a7c876513694ff45ce6a808ab54af0fca71d533
James Hanselman <68711757+JamesHansIV@users.noreply.github.com>
2024-03-24 00:28:57 -0500

    added multiselect cursor

--- src/Engine/Cursors/Cursor.cpp
+++ src/Engine/Cursors/Cursor.cpp
@@ -23,14 +23,26 @@ SDL_Rect Cursor::UpdateAndGetRect() {
     return m_Cursor_Rect;
 }
 
+SDL_Rect Cursor::UpdateAndGetRect(int offsetX, int offsetY) {
+    ImGuiIO const& io = ImGui::GetIO();  
+
+    m_Cursor_Rect.h = m_Height * m_Scale;
+    m_Cursor_Rect.w = m_Width * m_Scale;
+    m_Cursor_Rect.x = io.MousePos.x + offsetX;
+    m_Cursor_Rect.y = io.MousePos.y - m_Cursor_Rect.h + offsetY;
+
+    return m_Cursor_Rect;
+}
+
 std::string Cursor::GetTextureId(int  /*edit_mode*/) {
     return cursor_type_to_texture_id_map[m_Cursor_Type];
 }
 
 void Cursor::SetCursor(CursorType cursor_type) {
     m_Cursor_Type = cursor_type;
+    // m_Y_Offset = ;
 }
 
-void Cursor::SetCursor(int edit_mode) {
+void Cursor::SetCursor(EditMode edit_mode) {
     m_Cursor_Type = edit_mode_to_cursor_type_map[edit_mode];
 }
--- src/Engine/Cursors/Cursor.h
+++ src/Engine/Cursors/Cursor.h
@@ -4,10 +4,13 @@
 #include <unordered_map>
 #include <SDL2/SDL.h>
 
+#include "Engine/Editor/EditMode.h"
+
 enum class CursorType {
     POINT = 0,
     DRAW,
-    ERASE
+    ERASE,
+    MULTI_SELECT
 };
 
 
@@ -21,10 +24,13 @@ class Cursor {
 
         void Draw();
         void SetCursor(CursorType cursor_type);
-        void SetCursor(int edit_mode);
+        void SetCursor(EditMode edit_mode);
 
         SDL_Rect UpdateAndGetRect();
+        SDL_Rect UpdateAndGetRect(int offsetX, int offsetY);
         std::string GetTextureId(int edit_mode);
+
+        CursorType GetCursorType() {return m_Cursor_Type;}
 
 
 
@@ -32,19 +38,34 @@ class Cursor {
         SDL_Rect m_Cursor_Rect;
         int m_Width;
         int m_Height;
+        // int m_Offset_X;
+        // int m_Offset_Y;
         double m_Scale;
         CursorType m_Cursor_Type;
 
-        std::unordered_map<int, CursorType>edit_mode_to_cursor_type_map{
-            {0, CursorType::POINT}, 
-            {1, CursorType::DRAW}, 
-            {2, CursorType::ERASE},
-            {3, CursorType::POINT}
+        // std::unordered_map<int, CursorType>edit_mode_to_cursor_type_map{
+        //     {0, CursorType::POINT}, 
+        //     {1, CursorType::DRAW}, 
+        //     {2, CursorType::ERASE},
+        //     {3, CursorType::POINT},
+        //     {4, CursorType::POINT},
+        //     {5, CursorType::POINT}
+        // };
+
+        std::unordered_map<EditMode, CursorType>edit_mode_to_cursor_type_map {
+            {EditMode::NONE, CursorType::POINT}, 
+            {EditMode::DRAW, CursorType::DRAW}, 
+            {EditMode::ERASE, CursorType::ERASE},
+            {EditMode::OBJECT_SELECT, CursorType::POINT},
+            {EditMode::TILE_SELECT, CursorType::MULTI_SELECT},
+            {EditMode::LAYER_SELECT, CursorType::POINT},
+            {EditMode::DRAG_MOVE, CursorType::DRAW}
         };
 
         std::unordered_map<CursorType, std::string>cursor_type_to_texture_id_map {
             {CursorType::POINT, "editor-cursor-point"},
             {CursorType::DRAW, "editor-cursor-draw"},
-            {CursorType::ERASE, "editor-cursor-erase"}
+            {CursorType::ERASE, "editor-cursor-erase"},
+            {CursorType::MULTI_SELECT, "editor-cursor-multi-select"}
         };
 };
