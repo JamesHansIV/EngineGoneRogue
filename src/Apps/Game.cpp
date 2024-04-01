c5acfef86247e4dc854ad8d39f4553d500802c78
Minwoo Lee <leejoseph892@gmail.com>
2024-03-31 22:50:49 -0500

    add destructible

--- src/Apps/Game.cpp
+++ src/Apps/Game.cpp
@@ -235,7 +235,7 @@ void Game::GenerateRandomEnemy() {
     float const generated_y = rand() % 1800 + 20;
 
     // Generate random enemy type
-    int const enemy_type = 8;
+    int const enemy_type = rand() % 8;
     Enemy* generated_enemy = nullptr;
 
     switch (enemy_type) {
